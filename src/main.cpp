d27e202a11da4704a2762635668a33c21ec0131b
jcarolinares_ntt <julian.caro@nttdisruption.com>
2021-04-03 13:46:38 +0200

    IFTTT Push button working version

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,72 @@
+//Libraries
 #include <Arduino.h>
-
+#include <WiFi.h>
+#include <HTTPClient.h>
+ 
+ // Pinout
 #define YAHOO_PIN 13
 #define YAHOO_SWITCH 21
+
+// Wifi credentials
+const char* ssid = "<SSID>";
+const char* password = "<PASSWORD>";
+const char* IFTTT_URL = "<IFTT_URL>";
+
+// Functions declaration 
+int ifttt_post();
+
+int ifttt_post(){
+  if(WiFi.status()== WL_CONNECTED){   // Check WiFi connection status
+
+     HTTPClient http;
+     http.begin(IFTTT_URL); // Specify destination for HTTP request
+     http.addHeader("Content-Type", "text/plain"); //Specify content-type header
+
+     int httpResponseCode = http.POST("post");   // Send the actual POST request
+
+     if(httpResponseCode>0){
+      Serial.println(httpResponseCode);   // Print return code
+      return httpResponseCode;
+     }else{
+      Serial.print("Error on sending request: ");
+      Serial.println(httpResponseCode);
+      return httpResponseCode;
+     }
+
+     http.end();  //Free resources
+  }
+  else{
+    Serial.println("Connection error");
+    return -1;
+  }
+}
+
 
 void setup() {
 
   Serial.begin(115200);
-  // put your setup code here, to run once:
   pinMode(YAHOO_PIN,OUTPUT);
   pinMode(YAHOO_SWITCH,INPUT_PULLUP);
+
+  //Wifi connection
+  Serial.begin(115200);
+  delay(10);
+  Serial.print("Connecting");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+      Serial.print(".");
+  }
+
+  if(WiFi.status()== WL_CONNECTED){
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  else{
+    Serial.println("Not connected- Working without WIFI");
+  }
 }
 
 void loop() {
@@ -17,6 +75,7 @@ void loop() {
   Serial.println(switch_yahoo);
   if (switch_yahoo== LOW){
     digitalWrite(YAHOO_PIN, HIGH);
+    ifttt_post();
     delay(1000);
     digitalWrite(YAHOO_PIN, LOW);
     delay(200);
@@ -24,3 +83,4 @@ void loop() {
 
 delay(100);
 }
+
